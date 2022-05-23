de61c2994260ae4d9eb84c30dc91862b7ef088db
abxda <abxda@outlook.com>
2022-05-23 17:10:29 -0500

    timeout compose

--- scripts/myenvvars.sh
+++ scripts/myenvvars.sh
@@ -1,3 +1,4 @@
 export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:9000}
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=/usr/local/hadoop/bin:$PATH
+export COMPOSE_HTTP_TIMEOUT=30000
